67a40f400961447783e1b5d491aa7c6c6662824a
GittoKia <350207361@gapps.yrdsb.ca>
2025-05-27 23:44:42 -0400

    Update HomePage.tsx
    Linked backend routes to homepage

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,9 +1,18 @@
-import topicsData from '../topics.json'
 import TopicCard from '../components/TopicCard'
 import { type Topic } from '../types'
 import '../styles/HomePage.scss'
+import {useState,useEffect } from 'react'
+import {getTopics} from '../api.js'
 const HomePage = () => {
-    const topics: Topic[] = topicsData.topics
+    const [topics, setTopics] = useState([])
+
+
+    useEffect(()=>{async function loadAllPosts(){
+let data=await getTopics()
+if (data){
+    setTopics(data)
+}
+    }loadAllPosts()},[])
     return (
         <div className='Cards'>
             {topics.map((t) => (
