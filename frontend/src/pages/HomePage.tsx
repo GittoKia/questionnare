a2bb03a9fa4585b9c6979cc3a434357148c0ad90
GittoKia <350207361@gapps.yrdsb.ca>
2025-05-28 22:56:40 -0400

    Update HomePage.tsx

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,19 +1,16 @@
 import TopicCard from '../components/TopicCard'
-import { type Topic } from '../types'
 import { useState, useEffect } from 'react'
 import { getTopics } from '../api'
 import '../styles/HomePage.scss'
-import {useState,useEffect } from 'react'
-import {getTopics} from '../api.js'
 const HomePage = () => {
 
     //Code for backend route
-    const [tees, setTees] = useState([])
+    const [topics, setTopics] = useState([])
     useEffect(() => {
         async function loadAllPosts() {
             let data = await getTopics()
             if (data) {
-                setTees(data)
+                setTopics(data)
             }
         } loadAllPosts()
     }, [])
